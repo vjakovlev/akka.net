fb24576fe59a154d7fa4a71edc86bc7660e87c12
vjakovlev <vjakovlev89@gmail.com>
2023-06-08 13:47:11 +0200

    updates

---
+++ MovieStreaming/MovieStreamingFramework.Remote/Program.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreamingFramework.Remote
+{
+    internal class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+            //ColorConsole.WrtieLineGray("Creating MovieStreamingSystem in remote process");
+
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            MovieStreamingActorSystem.AwaitTermination();
+        }
+    }
+}
